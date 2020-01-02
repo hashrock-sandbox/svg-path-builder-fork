2ca1b4345ade4ccba75029de69b213d076807d41
hashrock <hashrock@users.noreply.github.com>
2020-01-02 23:44:15 +0900

    more preview size

--- src/result.jsx
+++ src/result.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Result(props) {
-  const output = `<svg viewBox="0 0 640 640" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="40">
+  const output = `<svg viewBox="0 0 640 640" width="24" height="24" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="60">
     <path d="${props.path}"  />
 </svg>`;
 
@@ -10,17 +10,48 @@ export default function Result(props) {
       <div className="ad-Result-preview">
         <svg
           viewBox="0 0 640 640"
+          width="16"
+          height="16"
+          fill="none"
+          stroke="currentcolor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="60"
+        >
+          <path d={props.path} />
+        </svg>
+
+        <span className="ad-Result-label">16px</span>
+
+        <svg
+          viewBox="0 0 640 640"
+          width="24"
+          height="24"
+          fill="none"
+          stroke="currentcolor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="60"
+        >
+          <path d={props.path} />
+        </svg>
+
+        <span className="ad-Result-label">24px</span>
+
+        <svg
+          viewBox="0 0 640 640"
           width="32"
           height="32"
           fill="none"
           stroke="currentcolor"
           strokeLinecap="round"
           strokeLinejoin="round"
-          strokeWidth="40"
+          strokeWidth="60"
         >
           <path d={props.path} />
         </svg>
-        <span className="ad-Result-label">Icon Preview</span>
+
+        <span className="ad-Result-label">32px</span>
       </div>
       <textarea
         className="ad-Result-textarea"
